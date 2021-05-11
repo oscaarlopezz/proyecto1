77629f6a1dd7d62d911949240c7383965abb7773
oscaarlopezz <oscarlm27@gmail.com>
2021-05-11 10:26:15 +0200

    oscar

--- view/datos.php
+++ view/datos.php
@@ -10,9 +10,18 @@ include_once '../services/conexion.php';
     <meta charset="UTF-8">
     <title>Gestion Escuela</title>
     <meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1.0, maximum-scale=3.0, minimum-scale=1.0">
+    <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <!-- ********* -->
+    <!-- Fuente -->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@300&display=swap" rel="stylesheet">
+    <!-- ****** -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
     <link rel="stylesheet" href="../css/stylesdatos.css">
-
 </head>
 <body>
     <div class="two-columns">
