2024dc0e2343d1723526f12f2882d79a01980e78
oscaarlopezz <oscarlm27@gmail.com>
2021-05-11 12:35:14 +0200

    oscar

--- services/crearuser.php
+++ services/crearuser.php
@@ -4,26 +4,66 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <title>Creación de usuarios</title>
-    <meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1.0, maximum-scale=3.0, minimum-scale=1.0">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <!-- ********* -->
+    <!-- Fuente -->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@300&display=swap" rel="stylesheet">
+    <!-- ****** -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
-    <link rel="stylesheet" href="../css/stylescrear.css">
 </head>
 
 <body>
-    <br>
-    <form action="recibircrear.php" method="POST">
-        <p>Nombre: <input type="text" name="nombre" size="40"></p>
-        <p>Apellido: <input type="text" name="apellido" size="40"></p>
-        <p>Curso: <input type="text" name="curso" size="40"></p>
-        <p>Grupo: <input type="text" name="grupo" size="40"></p>
-        <p>Edad: <input type="text" name="edad" size="40"></p>
-            <p>
-                <input type="submit" value="Enviar">
-                <input type="reset" value="Borrar">
-            </p>
+    <div class="container">
+    <form action="recibirdatos.php" method="get">
+    <div class="form-group">
+    <br/>
+    <div class="row">
+        <p>
+            <label for="text">DNI: </label>
+            <input type="text" class="form-control" name="servidor" size="40">
         </p>
+            <div class="col">
+                <label for="usr">Nombre: </label>
+                <input type="text" class="form-control" name="user" size="40">
+            </div>
+    </div>    
+    <div class="row">
+            <div class="col">
+                <label for="pwd">1º Apellido: </label>
+                <input type="text" class="form-control" name="passwd" size="40">
+            </div>
+        <p>
+            <label for="text">2º Apellido: </label>
+            <input type="text" class="form-control" name="bd" size="40"></p>
+    </div>
+    <div class="row">
+            <div class="col">
+                <label for="pwd">Correo: </label>
+                <input type="text" class="form-control" name="passwd" size="40">
+            </div>
+        <p>
+            <label for="text">Teléfono: </label>
+            <input type="text" class="form-control" name="bd" size="40"></p>
+        <p>
+    </div>
+    <p>
+        <label for="text">Clase: </label>
+        <input type="text" class="form-control" name="bd" size="40"></p>
+    <p>
+        <div class="col-sm-offset-2 col-sm-10">
+            <input type="submit" class="btn btn-warning" value="Enviar">
+            <input type="reset" class="btn btn-danger" value="Borrar">
+        </div>
+        </p>
+    </div>
     </form>
+</div>
+
 </body>
 </body>
