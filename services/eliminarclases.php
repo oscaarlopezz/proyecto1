8e57b5f2ff0e15aab3963be54fa27930e591e007
oscaarlopezz <oscarlm27@gmail.com>
2021-05-13 09:34:16 +0200

    oscar

--- services/eliminarclases.php
+++ services/eliminarclases.php
@@ -2,10 +2,10 @@
 
 include 'conexion.php';
 
-$id=$_GET['id'];
+$id=$_GET['id_classe'];
 echo "Tu id es $id";
 
 //$conexion = mysqli_connect("localhost","root","","bd_escuela");
 
 $delete = mysqli_query($conexion,"DELETE FROM tbl_classe Where id_classe=$id");
-header("Location:datos.php");
+header("Location:../view/datosCl.php");
